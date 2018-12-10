09ad56ceaddbb68bdba95dea013bccc552bf3563
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-12-10 22:12:30 +0900

    update devDependencies
    Highlights:
    
    * update ESLint from v0.x to v5.x
    * introduce @shinnn/eslint-config and follow its rules, for example tab indent
    * introduce Rollup and rollup-config-module to adapt to .mjs-based ecosystem
    * switch from istanbul to nyc

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/*!
+ * block-comment | MIT (c) Shinnosuke Watanabe
+ * https://github.com/shinnn/block-comment
+*/
+
+function blockComment(lines, option) {
+	option = option || {};
+	if (!Array.isArray(lines)) {
+		lines = lines.split(/\r?\n/);
+	}
+
+	var content = '';
+
+	for (var i = 0; i < lines.length; i++) {
+		var prefix = blockComment.linePrefix;
+		if (lines[i] !== '') {
+			prefix += ' ';
+		}
+
+		content += prefix + lines[i] + '\n';
+	}
+
+	var start = '';
+
+	if (option.start !== undefined) {
+		if (/\r?\n/.test(option.start)) {
+			throw new Error('start option must not include newlines.');
+		}
+
+		start = option.start;
+	}
+
+	return blockComment.open + start + '\n' + content + blockComment.close;
+}
+
+blockComment.open = '/*';
+blockComment.linePrefix = ' *';
+blockComment.close = '*/';
+
+module.exports = blockComment;
--- test.js
+++ test.js
@@ -3,79 +3,67 @@
 const blockComment = require('.');
 const test = require('tape');
 
-test(description, function(t) {
-  t.equal(blockComment('a'), [
-    '/*',
-    ' * a',
-    '*/'
-  ].join('\n'), 'should create a comment from a string.');
+test('blockComment()', t => {
+	t.equal(blockComment('a'), `/*
+ * a
+*/`, 'should return a comment from a string.');
 
-  t.equal(blockComment('a\nb\nc'), [
-    '/*',
-    ' * a',
-    ' * b',
-    ' * c',
-    '*/'
-  ].join('\n'), 'should return a multi-line comment.');
+	t.equal(blockComment('a\nb\nc'), `/*
+ * a
+ * b
+ * c
+*/`, 'should return a multiline comment.');
 
-  t.equal(blockComment('a\nb\r\nc'), [
-    '/*',
-    ' * a',
-    ' * b',
-    ' * c',
-    '*/'
-  ].join('\n'), 'should normalize newlines.');
+	t.equal(blockComment('a\nb\r\nc'), `/*
+ * a
+ * b
+ * c
+*/`, 'should normalize newlines.');
 
-  t.equal(blockComment(['a', 'b']), [
-    '/*',
-    ' * a',
-    ' * b',
-    '*/'
-  ].join('\n'), 'should create a comment from an array.');
+	t.equal(blockComment(['a', 'b']), `/*
+ * a
+ * b
+*/`, 'should return a comment from an array.');
 
-  t.equal(blockComment(''), [
-    '/*',
-    ' *',
-    '*/'
-  ].join('\n'), 'should accept an empty string.');
+	t.equal(blockComment(''), `/*
+ *
+*/`, 'should accept an empty string.');
 
-  t.equal(blockComment([]), [
-    '/*',
-    '*/'
-  ].join('\n'), 'should accept an empty array.');
+	t.equal(blockComment([]), `/*
+*/`, 'should accept an empty array.');
 
-  t.equal(blockComment('a\nb', {start: '!'}), [
-    '/*!',
-    ' * a',
-    ' * b',
-    '*/'
-  ].join('\n'), 'should prepend a string using `start` option.');
+	t.equal(blockComment('a\nb', {start: '!'}), `/*!
+ * a
+ * b
+*/`, 'should prepend a string using `start` option.');
 
-  blockComment.open = 'a';
-  blockComment.linePrefix = 'b';
-  blockComment.close = 'c';
+	blockComment.open = 'a';
+	blockComment.linePrefix = 'b';
+	blockComment.close = 'c';
 
-  t.equal(blockComment(''), [
-    'a',
-    'b',
-    'c'
-  ].join('\n'), 'should modify template using its properties.');
+	t.equal(
+		blockComment(''),
+		'a\nb\nc',
+		'should modify template using its properties.'
+	);
 
-  t.throws(
-    blockComment.bind(null, 123),
-    /TypeError/,
-    'should throw an error when the first argument is not a string or an array.'
-  );
+	t.throws(
+		() => blockComment(1),
+		/^TypeError/u,
+		'should throw an error when the first argument is not a string or an array.'
+	);
 
-  t.throws(
-    () => blockComment('a', {start: 'b\n'}),
-    /must not include newline/u,
-    'should throw an error when `start` option includes \'\\n\'.'
-  );
+	t.throws(
+		() => blockComment('a', {start: 'b\n'}),
+		/^Error.*start option must not include newlines\./u,
+		'should throw an error when `start` option includes \'\\n\'.'
+	);
 
-  t.throws(
-    () => blockComment('a', {start: 'b\r\n'}),
-    /must not include newline/u,
-    'should throw an error when `start` option includes \'\\r\\n\'.'
-  );
+	t.throws(
+		() => blockComment('a', {start: 'b\r\n'}),
+		/^Error.*start option must not include newlines\./u,
+		'should throw an error when `start` option includes \'\\r\\n\'.'
+	);
+
+	t.end();
 });
